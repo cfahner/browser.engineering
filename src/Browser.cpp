6cee9ff47c7f888f273422a0a442a13d119b8cc8
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-07-19 23:35:48 +0200

    Updates BrBrowser to support both file: and data: URLs

--- src/Browser.cpp
+++ src/Browser.cpp
@@ -4,6 +4,7 @@
  */
 #include <string>
 
+#include "HTTP/Header.h"
 #include "HTTP/Method.h"
 #include "HTTP/NetworkException.h"
 #include "HTTP/Request.h"
@@ -12,13 +13,9 @@
 #include "HTTP/URLParseException.h"
 #include "Browser.h"
 
-void Browser::visit(std::string url_string) {
+void Browser::visit(std::string_view url_string) {
 	try {
-		HTTP::Request request{
-			HTTP::Method::GET,
-			HTTP::URL::parse_string(url_string)
-		};
-		HTTP::Response response{request.send()};
+		HTTP::Response response{fetch_url(url_string)};
 		for (const HTTP::Header& header : response.headers) {
 			std::cout << static_cast<std::string>(header) << std::endl;
 		}
@@ -27,4 +24,18 @@ void Browser::visit(std::string url_string) {
 	} catch (HTTP::URLParseException& upe) {
 		std::cout << "Could not parse URL: " << upe.what() << std::endl;
 	}
+}
+
+HTTP::Response Browser::fetch_url(std::string_view url_string) {
+	if (url_string.length() >= 5 && url_string.substr(0, 5) == "data:") {
+		return HTTP::Response::from_data_uri(url_string);
+	}
+	HTTP::Request request = create_request(HTTP::Method::GET, HTTP::URL::parse_string(url_string));
+	return request.send();
+}
+
+HTTP::Request Browser::create_request(HTTP::Method method, HTTP::URL url) {
+	HTTP::Request request{method, url};
+	request.headers.push_back(HTTP::Header{"user-agent", "CFahnerBrowser"});
+	return request;
 }
--- src/Browser.h
+++ src/Browser.h
@@ -6,10 +6,16 @@
 
 #include <string>
 
+#include "HTTP/Request.h"
+#include "HTTP/Method.h"
 #include "HTTP/URL.h"
 
 class Browser {
 public:
-	void visit(std::string url_string);
+	void visit(std::string_view url_string);
+
+private:
+	HTTP::Request create_request(HTTP::Method method, HTTP::URL url);
+	HTTP::Response fetch_url(std::string_view url_string);
 
 };
