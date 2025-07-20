78c01bd017744c6e1087c0bc891b52376f8d0f87
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-07-20 12:01:24 +0200

    Modifies various explanations of CURLcodes in CURLException

--- src/HTTP/CURLException.cpp
+++ src/HTTP/CURLException.cpp
@@ -14,10 +14,10 @@ CURLException::CURLException(CURLcode curl_code)
 	, curl_code{curl_code}
 {
 	curl_code = curl_code;
-	std::string error{"Network exception (CURL): "};
+	std::string error{"CURL exception ("};
 	switch (curl_code) {
-		case CURLE_UNSUPPORTED_PROTOCOL:
-			error += "unsupported protocol";
+		case CURLE_UNSUPPORTED_PROTOCOL: // 1
+			error += "protocol not supported or disallowed";
 			break;
 		case CURLE_URL_MALFORMAT: // 3
 			error += "malformed URL";
@@ -25,22 +25,25 @@ CURLException::CURLException(CURLcode curl_code)
 		case CURLE_COULDNT_RESOLVE_HOST: // 6
 			error += "failed to resolve host";
 			break;
-		case CURLE_COULDNT_CONNECT:
+		case CURLE_COULDNT_CONNECT: // 7
 			error += "failed to connect";
 			break;
-		case CURLE_OPERATION_TIMEDOUT:
+		case CURLE_OPERATION_TIMEDOUT: // 28
 			error += "timeout";
 			break;
-		case CURLE_SSL_CONNECT_ERROR:
+		case CURLE_SSL_CONNECT_ERROR: // 35
 			error += "failed SSL/TLS handshake";
 			break;
-		case CURLE_TOO_MANY_REDIRECTS:
+		case CURLE_TOO_MANY_REDIRECTS: // 47
 			error += "too many redirects";
+			break;
+		case CURLE_PEER_FAILED_VERIFICATION: // 60
+			error += "failed to verify certificate";
 			break;
 		default:
 			error += "unknown";
 	};
-	m_message = error;
+	m_message = error + ")";
 }
 
 }
