4eb70fc72daf6629584822cc3060f92d2a867e51
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-08-02 20:09:02 +0200

    Switches main() function to use the UI::Browser and requires a URL to be given when launching from command line

--- src/browse.cpp
+++ src/browse.cpp
@@ -2,12 +2,15 @@
  * Copyright (c) 2025, C. Fahner
  * MIT License
  */
+#include <iostream>
 
-#include <gtkmm/application.h>
+#include "UI/Browser.h"
 
-#include "Browser.h"
-
-int main(int argc, char* argv[]) {
-	auto app = Gtk::Application::create("com.frisiapp.cf.browser-engineering");
-	return app->make_window_and_run<Browser>(argc, argv);
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		std::cerr << "Please pass the URL to open as the first argument" << std::endl;
+		return 1;
+	}
+	auto browser = UI::Browser::create(argv[1]);
+	return browser->run();
 }
