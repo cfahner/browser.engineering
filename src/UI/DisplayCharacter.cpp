9047ed6db126ce13071e932c0d65e73ded3800a9
C. Fahner <cfahner.git.iech6cee@frisiapp.com>
2025-08-17 07:40:04 +0200

    Removes DisplayCharacter as it has been replaced by DisplaySpan (and Span)

--- src/UI/DisplayCharacter.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2025, C. Fahner
- * MIT License
- */
-#include "DisplayCharacter.h"
-
-std::ostream& operator<<(std::ostream& out, const UI::DisplayCharacter& display_character) {
-	return out << "DisplayCharacter { " << display_character.m_x << ", " << display_character.m_y << " }";
-}
